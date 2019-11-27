d8b314e941cee8845fc3b00a3da31b8905daa7f7
Eliza Weisman <eliza@buoyant.io>
2019-11-27 11:26:44 -0800

    frames are now pages
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- hal-core/src/mem/frame.rs
+++ hal-core/src/mem/frame.rs
@@ -2,70 +2,70 @@ use crate::{Address, Architecture};
 use core::{cmp, fmt, marker::PhantomData, ops, slice};
 
 pub trait Size: Copy + PartialEq + Eq {
-    /// The size (in bits) of this frame.
+    /// The size (in bits) of this page.
     const SIZE: usize;
 }
 
-/// An allocator for physical frames of a given size.
+/// An allocator for physical pages of a given size.
 ///
 /// # Safety
 ///
 /// This trait is unsafe to implement, as implementations are responsible for
-/// guaranteeing that allocated frames are unique, and may not be allocated by
-/// another frame allocator.
+/// guaranteeing that allocated pages are unique, and may not be allocated by
+/// another page allocator.
 pub unsafe trait Alloc<A: Architecture, S: Size> {
-    /// Allocate a single frame.
+    /// Allocate a single page.
     ///
     /// Note that an implementation of this method is provided as long as an
     /// implementor of this trait provides `alloc_range`.
     ///
     /// # Returns
-    /// - `Ok(Frame)` if a frame was successfully allocated.
-    /// - `Err` if no more frames can be allocated by this allocator.
-    fn alloc(&mut self) -> Result<Frame<A::PAddr, S>, AllocErr> {
+    /// - `Ok(Page)` if a page was successfully allocated.
+    /// - `Err` if no more pages can be allocated by this allocator.
+    fn alloc(&mut self) -> Result<Page<A::PAddr, S>, AllocErr> {
         self.alloc_range(1).map(|r| r.start())
     }
 
-    /// Allocate a range of `len` frames.
+    /// Allocate a range of `len` pages.
     ///
     /// # Returns
-    /// - `Ok(FrameRange)` if a range of frames was successfully allocated
+    /// - `Ok(PageRange)` if a range of pages was successfully allocated
     /// - `Err` if the requested range could not be satisfied by this allocator.
-    fn alloc_range(&mut self, len: usize) -> Result<FrameRange<A::PAddr, S>, AllocErr>;
+    fn alloc_range(&mut self, len: usize) -> Result<PageRange<A::PAddr, S>, AllocErr>;
 
-    /// Deallocate a single frame.
+    /// Deallocate a single page.
     ///
     /// Note that an implementation of this method is provided as long as an
     /// implementor of this trait provides `dealloc_range`.
     ///
     /// # Returns
-    /// - `Ok(())` if the frame was successfully deallocated.
+    /// - `Ok(())` if the page was successfully deallocated.
     /// - `Err` if the requested range could not be deallocated.
-    fn dealloc(&mut self, frame: Frame<A::PAddr, S>) -> Result<(), AllocErr> {
-        self.dealloc_range(frame.range_inclusive(frame))
+    fn dealloc(&mut self, page: Page<A::PAddr, S>) -> Result<(), AllocErr> {
+        self.dealloc_range(page.range_inclusive(page))
     }
 
-    /// Deallocate a range of frames.
+    /// Deallocate a range of pages.
     ///
     /// # Returns
-    /// - `Ok(())` if a range of frames was successfully deallocated
+    /// - `Ok(())` if a range of pages was successfully deallocated
     /// - `Err` if the requested range could not be deallocated.
-    fn dealloc_range(&self, range: FrameRange<A::PAddr, S>) -> Result<(), AllocErr>;
+    fn dealloc_range(&self, range: PageRange<A::PAddr, S>) -> Result<(), AllocErr>;
 }
 
-/// A memory frame.
+/// A memory page.
 #[derive(Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
-pub struct Frame<A, S: Size> {
+pub struct Page<A, S: Size> {
     base: A,
     _size: PhantomData<S>,
 }
 
-/// A range of memory frames of the same size.
+/// A range of memory pages of the same size.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct FrameRange<A: Address, S: Size> {
-    start: Frame<A, S>,
-    end: Frame<A, S>,
+pub struct PageRange<A: Address, S: Size> {
+    start: Page<A, S>,
+    end: Page<A, S>,
 }
 
 pub struct NotAligned<S> {
@@ -77,10 +77,10 @@ pub struct AllocErr {
     _p: (),
 }
 
-// === impl Frame ===
+// === impl Page ===
 
-impl<A: Address, S: Size> Frame<A, S> {
-    /// Returns a frame starting at the given address.
+impl<A: Address, S: Size> Page<A, S> {
+    /// Returns a page starting at the given address.
     pub fn starting_at(addr: impl Into<A>) -> Result<Self, NotAligned<S>> {
         let addr = addr.into();
         if !addr.is_aligned(S::SIZE) {
@@ -89,7 +89,7 @@ impl<A: Address, S: Size> Frame<A, S> {
         Ok(Self::containing(addr))
     }
 
-    /// Returns the frame that contains the given address.
+    /// Returns the page that contains the given address.
     pub fn containing(addr: impl Into<A>) -> Self {
         let base = addr.into().align_down(S::SIZE);
         Self {
@@ -111,78 +111,78 @@ impl<A: Address, S: Size> Frame<A, S> {
         addr >= self.base && addr <= self.end_address()
     }
 
-    pub fn range_inclusive(self, end: Frame<A, S>) -> FrameRange<A, S> {
-        FrameRange { start: self, end }
+    pub fn range_inclusive(self, end: Page<A, S>) -> PageRange<A, S> {
+        PageRange { start: self, end }
     }
 
-    pub fn range_to(self, end: Frame<A, S>) -> FrameRange<A, S> {
-        FrameRange {
+    pub fn range_to(self, end: Page<A, S>) -> PageRange<A, S> {
+        PageRange {
             start: self,
             end: end - 1,
         }
     }
 }
 
-impl<A: Address, S: Size> ops::Add<usize> for Frame<A, S> {
+impl<A: Address, S: Size> ops::Add<usize> for Page<A, S> {
     type Output = Self;
     fn add(self, rhs: usize) -> Self {
-        Frame {
+        Page {
             base: self.base + (S::SIZE * rhs),
             _size: PhantomData,
         }
     }
 }
 
-impl<A: Address, S: Size> ops::Sub<usize> for Frame<A, S> {
+impl<A: Address, S: Size> ops::Sub<usize> for Page<A, S> {
     type Output = Self;
     fn sub(self, rhs: usize) -> Self {
-        Frame {
+        Page {
             base: self.base - (S::SIZE * rhs),
             _size: PhantomData,
         }
     }
 }
 
-impl<A: Address, S: Size> AsRef<[u8]> for Frame<A, S> {
+impl<A: Address, S: Size> AsRef<[u8]> for Page<A, S> {
     fn as_ref(&self) -> &[u8] {
         let start = self.base.as_ptr() as *const u8;
         unsafe { slice::from_raw_parts::<u8>(start, S::SIZE) }
     }
 }
 
-impl<A: Address, S: Size> cmp::PartialOrd for Frame<A, S> {
+impl<A: Address, S: Size> cmp::PartialOrd for Page<A, S> {
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        // Since the frames are guaranteed by the type system to be the same
+        // Since the pages are guaranteed by the type system to be the same
         // we can simply compare the base addresses.
         self.base.partial_cmp(&other.base)
     }
 }
 
-impl<A: Address, S: Size> cmp::Ord for Frame<A, S> {
+impl<A: Address, S: Size> cmp::Ord for Page<A, S> {
     fn cmp(&self, other: &Self) -> cmp::Ordering {
-        // Since the frames are guaranteed by the type system to be the same
+        // Since the pages are guaranteed by the type system to be the same
         // we can simply compare the base addresses.
         self.base.cmp(&other.base)
     }
 }
 
-impl<A: fmt::Debug, S: Size> fmt::Debug for Frame<A, S> {
+impl<A: fmt::Debug, S: Size> fmt::Debug for Page<A, S> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Frame")
+        f.debug_struct("Page")
             .field("base", &self.base)
             .field("size", &S::SIZE)
             .finish()
     }
 }
 
-// === impl FrameRange ===
+// === impl PageRange ===
 
-impl<A: Address, S: Size> FrameRange<A, S> {
-    pub fn start(&self) -> Frame<A, S> {
+impl<A: Address, S: Size> PageRange<A, S> {
+    pub fn start(&self) -> Page<A, S> {
         self.start
     }
 
-    pub fn end(&self) -> Frame<A, S> {
+    pub fn end(&self) -> Page<A, S> {
         self.end
     }
 
@@ -191,8 +191,8 @@ impl<A: Address, S: Size> FrameRange<A, S> {
     }
 }
 
-impl<A: Address, S: Size> Iterator for FrameRange<A, S> {
-    type Item = Frame<A, S>;
+impl<A: Address, S: Size> Iterator for PageRange<A, S> {
+    type Item = Page<A, S>;
     fn next(&mut self) -> Option<Self::Item> {
         if self.start > self.end {
             return None;
