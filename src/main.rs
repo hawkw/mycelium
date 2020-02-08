ffdb279fdb4dc80618f2a46073ddf41b580d8cb9
Nika Layzell <nika@thelayzells.com>
2020-02-08 13:26:29 -0500

    silence clippy missing main warning

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+#![no_main]
 #![cfg_attr(target_os = "none", no_std)]
-#![cfg_attr(target_os = "none", no_main)]
 #![cfg_attr(target_os = "none", feature(alloc_error_handler))]
 #![cfg_attr(target_os = "none", feature(asm))]
 #![cfg_attr(target_os = "none", feature(panic_info_message, track_caller))]
