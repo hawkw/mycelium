e7b8ca0f0713597c5b22a96ae694fa7cd4a5b7b9
Eliza Weisman <eliza@buoyant.io>
2020-05-02 09:07:16 -0700

    feat(util): add exponential backoff primitive
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- util/src/sync/mod.rs
+++ util/src/sync/mod.rs
@@ -7,3 +7,44 @@ pub use loom::sync::atomic;
 pub use core::sync::atomic;
 
 pub mod spin;
+
+/// An exponential backoff for spin loops
+#[derive(Debug, Clone)]
+pub(crate) struct Backoff {
+    exp: u8,
+    max: u8,
+}
+
+impl Backoff {
+    pub(crate) const DEFAULT_MAX_EXPONENT: u8 = 8;
+
+    pub(crate) const fn new() -> Self {
+        Self { exp: 0, max: 8 }
+    }
+
+    /// Returns a new exponential backoff with the provided max exponent.
+    #[allow(dead_code)]
+    pub(crate) fn with_max_exponent(max: u8) -> Self {
+        assert!(max <= 8);
+        Self { exp: 0, max }
+    }
+
+    /// Perform one spin, squarin the backoff
+    #[inline(always)]
+    pub(crate) fn spin(&mut self) {
+        // Issue 2^exp pause instructions.
+        for _ in 0..(1 << self.exp) {
+            atomic::spin_loop_hint();
+        }
+
+        if self.exp < self.max {
+            self.exp += 1
+        }
+    }
+}
+
+impl Default for Backoff {
+    fn default() -> Self {
+        Self::new()
+    }
+}
