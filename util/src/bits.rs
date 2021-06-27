ed0de4a2bfbf4c6fe15981149c25e83af21a4ad1
Eliza Weisman <eliza@buoyant.io>
2021-06-27 11:35:09 -0700

    add `Pack::pair_with`
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- util/src/bits.rs
+++ util/src/bits.rs
@@ -292,16 +292,31 @@ macro_rules! make_packers {
                 /// into another location, and vice versa.
                 pub const fn pair_at(&self, at: u32) -> $Pair {
                     let dst = Self::starting_at(at, self.bits());
-                    let at = at.saturating_sub(1);
-                    // TODO(eliza): validate that `at + self.bits() < N_BITS` in
+                    self.pair_with(dst)
+                }
+
+                /// Returns a pair type for packing bits from the range
+                /// specified by `self` after the specified packing spec.
+                pub const fn pair_after(&self, after: &Self) -> $Pair {
+                    self.pair_at(after.shift_next())
+                }
+
+                /// Returns a pair type for packing bits from the range
+                /// specified by `self` into the range specified by `with`.
+                ///
+                /// # Note
+                /// The two ranges must be the same size. This can be asserted
+                /// by the `assert_valid` method on the returned pair type.
+                pub const fn pair_with(&self, dst: &Self) -> $Pair {
+                    // TODO(eliza): validate that `dst.shift + self.bits() < N_BITS` in
                     // const fn somehow lol
-                    let (dst_shl, dst_shr) = if at > self.shift {
+                    let (dst_shl, dst_shr) = if dst.shift > self.shift {
                         // If the destination is greater than `self`, we need to
                         // shift left.
-                        ((at - self.shift) as $Bits, 0)
+                        ((dst.shift - self.shift) as $Bits, 0)
                     } else {
                         // Otherwise, shift down.
-                        (0, (self.shift - at) as $Bits)
+                        (0, (self.shift - dst.shift) as $Bits)
                     };
                     $Pair {
                         src: *self,
@@ -311,11 +326,6 @@ macro_rules! make_packers {
                     }
                 }
 
-                /// Returns a pair type for packing bits from the range
-                /// specified by `self` after the specified packing spec.
-                pub const fn pair_after(&self, after: &Self) -> $Pair {
-                    self.pair_at(after.shift_next())
-                }
 
                 /// Returns the number of bits needed to pack this value.
                 pub const fn bits(&self) -> u32 {
