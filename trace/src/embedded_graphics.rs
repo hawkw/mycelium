f03c0a65b80b1ee1310d7552b1622f3627a6a05e
Eliza Weisman <eliza@buoyant.io>
2021-10-09 14:27:39 -0700

    comments
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- trace/src/embedded_graphics.rs
+++ trace/src/embedded_graphics.rs
@@ -44,14 +44,44 @@ where
         let curr_packed = self.mk.next_point.load(Ordering::Relaxed);
         let mut curr_point = unpack_point(curr_packed);
 
-        // The embedded-graphics crate doesn't handle strings beginning and
-        // ending with newlines, so we have to do it ourselves.
+        // for a couple of reasons, we don't trust the `embedded-graphics` crate
+        // to handle newlines for us:
+        //
+        // 1. it currently only actually produces a carriage return when a
+        //    newline character appears in the *middle* of a string. this means
+        //    that strings beginning or ending with newlines (and strings that
+        //    are *just* newlines) won't advance the write position the way we'd
+        //    expect them to. so, we have to do that part ourself --- it turns
+        //    out that most `fmt::Debug`/`fmt::Display` implementations will
+        //    write a bunch of strings that begin or end with `\n`.
+        // 2. when we reach the bottom of the screen, we want to scroll the
+        //    previous text up to make room for a new line of text.
+        //    `embedded-graphics` doesn't implement this behavior. because we
+        //    want to scroll every time we encounter a newline if we have
+        //    reached the bottom of the screen, this means we have to implement
+        //    *all* newline handling ourselves.
+        //
+        // TODO(eliza): currently, our newline handling doesn't honor
+        // configurable line height. all lines are simply a single character
+        // high. if we want to do something nicer about line height, we'd have
+        // to implement that here...
         for mut line in s.split_inclusive('\n') {
+            // does this chunk end with a newline? it might not, if:
+            // (a) it's the last chunk in a string where newlines only occur in
+            //     the beginning/middle.
+            // (b) the string being written has no newlines (so
+            //     `split_inclusive` will only yield a single chunk)
             let has_newline = line.ends_with('\n');
             if has_newline {
+                // if there's a trailing newline, trim it off --- no sense
+                // making the `embedded-graphics` crate draw an extra character
+                // it will essentially nop for.
                 line = &line[..line.len() - 1];
             }
 
+            // if we have reached the bottom of the screen, we'll need to scroll
+            // previous framebuffer contents up to make room for new line(s) of
+            // text.
             if curr_point.y > self.mk.last_line {
                 let ydiff = curr_point.y - self.mk.last_line;
                 curr_point = Point {
@@ -62,15 +92,18 @@ where
             }
 
             let next_point = if line.is_empty() {
+                // if this line is now empty, it was *just* a newline character,
+                // so all we have to do is advance the write position.
                 curr_point
             } else {
-                // Otherwise, actually draw the text.
+                // otherwise, actually draw the text.
                 Text::with_alignment(s, curr_point, default_text_style(), text::Alignment::Left)
                     .draw(&mut self.target)
                     .map_err(|_| fmt::Error)?
             };
 
             if has_newline {
+                // carriage return
                 curr_point = Point {
                     y: curr_point.y + self.mk.char_height as i32,
                     x: 10,
