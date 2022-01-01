e012c86a6bed37ed19f1f1b21e78630600a1ca23
Eliza Weisman <eliza@buoyant.io>
2022-01-01 10:42:16 -0800

    feat(inoculate): read cargo path with clap
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- inoculate/src/cargo.rs
+++ inoculate/src/cargo.rs
@@ -1,34 +1,13 @@
-use crate::{term::ColorMode, Result};
-use color_eyre::eyre::{ensure, WrapErr};
-use std::{env, path::PathBuf, process::Command};
+use crate::Options;
+use std::process::Command;
 
-pub fn cmd(cmd: &str) -> Result<Command> {
-    let cargo =
-        wheres_cargo().with_context(|| format!("failed to build `cargo {}` command", cmd))?;
-    let mut cargo = Command::new(cargo);
-    cargo
-        .arg(cmd)
-        // propagate our color mode configuration
-        .env("CARGO_TERM_COLOR", ColorMode::default().as_str());
-    Ok(cargo)
-}
-
-pub fn wheres_cargo() -> Result<PathBuf> {
-    if let Some(cargo) = env::var_os("CARGO") {
-        let cargo = PathBuf::from(cargo);
-        ensure!(
-            cargo.exists(),
-            "cargo path from CARGO env var ({}) does not exist",
-            cargo.display()
-        );
-        return Ok(cargo);
+impl Options {
+    pub fn cargo_cmd(&self, cmd: &str) -> Command {
+        let mut cargo = Command::new(self.cargo_path.as_os_str());
+        cargo
+            .arg(cmd)
+            // propagate our color mode configuration
+            .env("CARGO_TERM_COLOR", self.color.as_str());
+        cargo
     }
-
-    let cargo = PathBuf::from(env!("CARGO"));
-    ensure!(
-        cargo.exists(),
-        "cargo path from build-time CARGO env var ({}) does not exist",
-        cargo.display()
-    );
-    Ok(cargo)
 }
