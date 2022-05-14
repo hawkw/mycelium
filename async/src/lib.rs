c774b38c521dcdcfb34763d620a1e40cd21b6cac
Eliza Weisman <eliza@buoyant.io>
2022-05-14 10:32:51 -0700

    feat(async): improve tracing
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- async/src/lib.rs
+++ async/src/lib.rs
@@ -1,41 +1,9 @@
 #![cfg_attr(not(test), no_std)]
 extern crate alloc;
 
-macro_rules! event {
-    ($($arg:tt)*) => {
-        #[cfg(all(test, loom))]{
-            use tracing::Level;
-            tracing::event!($($arg)*);
-        }
-        #[cfg(all(test, loom))]
-        {
-            use tracing_01::Level;
-            tracing_01::event!($($arg)*);
-        }
-    }
-}
-
-#[cfg(not(test))]
-macro_rules! test_dbg {
-    ($e:expr) => {
-        $e
-    };
-}
-
-#[cfg(test)]
-macro_rules! test_dbg {
-    ($e:expr) => {
-        match $e {
-            e => {
-                event!(Level::DEBUG, "{} = {:?}", stringify!($e), &e);
-                e
-            }
-        }
-    };
-}
-
+#[macro_use]
+mod util;
 pub(crate) mod loom;
 
 pub mod scheduler;
 pub mod task;
-mod util;
