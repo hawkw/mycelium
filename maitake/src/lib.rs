64c42a13d8a930cecec4df4aa5052ceb10344633
James Munns <james@onevariable.com>
2022-05-31 21:45:47 +0200

    Update maitake/src/lib.rs
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- maitake/src/lib.rs
+++ maitake/src/lib.rs
@@ -1,8 +1,7 @@
 #![cfg_attr(docsrs, doc = include_str!("../README.md"))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg, doc_cfg_hide))]
-#![cfg_attr(not(test), no_std)]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg, doc_cfg_hide))]
 #![cfg_attr(docsrs, doc(cfg_hide(docsrs)))]
+#![cfg_attr(not(test), no_std)]
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
