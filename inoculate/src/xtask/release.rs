0abdb60bd908de2fd762ecc337465c925a8d1453
Eliza Weisman <eliza@buoyant.io>
2022-08-06 11:03:31 -0700

    wip release

--- inoculate/src/xtask/release.rs
+++ inoculate/src/xtask/release.rs
@@ -1,8 +1,10 @@
 use crate::{cli, Result};
+use cargo_metadata::Package;
 use color_eyre::{
     eyre::{self, WrapErr},
     Help,
 };
+use std::fs;
 
 /// Arguments for a release command.
 #[derive(Debug, clap::Subcommand)]
@@ -51,7 +53,12 @@ impl Cmd {
     pub(super) fn run(&self, opts: &cli::Options) -> Result<()> {
         match self {
             Cmd::UpdateChangelog(args) => args.update_changelog(opts),
-            Cmd::Release(args) => args.release(opts),
+            Cmd::Release(args) => args.release(opts).with_note(|| {
+                format!(
+                    "while releasing {} (version={:?})",
+                    args.crate_name, args.version
+                )
+            }),
         }
     }
 }
@@ -65,9 +72,9 @@ impl ReleaseArgs {
         (|| {
             tracing::info!(crate_name = %self.crate_name, version = ?self.version, "updating changelog");
             let metadata = metadata_for_crate(&self.crate_name, opts)?;
-            let version = self.version.increment(metadata.version);
+            let new_version = self.version.increment(&metadata.version);
+            self.write_changelog_update(&metadata, new_version)
 
-            eyre::bail!("can't update changelog, not yet implemented")
         })().with_context(|| {
             format!(
                 "failed to update changelog for {}",
@@ -76,23 +83,56 @@ impl ReleaseArgs {
         })
     }
 
+    fn write_changelog_update(
+        &self,
+        metadata: &Package,
+        new_version: semver::Version,
+    ) -> Result<()> {
+        eyre::bail!("can't update changelog, not yet implemented")
+    }
+
     #[tracing::instrument(
         level = "info", skip(self, opts),
         fields(%self.crate_name, ?self.version, self.dry_run = self.dry_run)
     )]
     fn release(&self, opts: &cli::Options) -> Result<()> {
-        (|| {
-            self.update_changelog(opts)?;
+        tracing::info!(crate_name = %self.crate_name, version = ?self.version, "releasing");
+        let metadata = metadata_for_crate(&self.crate_name, opts)?;
+        let new_version = self.version.increment(&metadata.version);
+        self.update_cargo_toml(&metadata, &new_version)?;
+        self.update_changelog(opts)?;
+        eyre::bail!("can't release, not yet implemented")
+    }
 
-            tracing::info!(crate_name = %self.crate_name, version = ?self.version, "releasing");
-            eyre::bail!("can't release, not yet implemented")
-        })()
-        .with_note(|| {
-            format!(
-                "while releasing {} (version={:?})",
-                self.crate_name, self.version
-            )
-        })
+    #[tracing::instrument]
+    fn update_cargo_toml(&self, metadata: &Package, version: &semver::Version) -> Result<()> {
+        let old_version = &metadata.version;
+        let cargo_toml_path = metadata.manifest_path.as_path();
+        tracing::info!(
+            new_version = %version,
+            %old_version,
+            "Updating {cargo_toml_path}"
+        );
+        let file = fs::read_to_string(cargo_toml_path).context("failed to read Cargo.toml")?;
+
+        let new_file = file.replace(
+            &format!("version = \"{}\"", old_version),
+            &format!("version = \"{}\"", version),
+        );
+
+        prettydiff::text::diff_lines(&file, &new_file)
+            .names("old Cargo.toml", "new Cargo.toml")
+            .set_diff_only(true)
+            .prettytable();
+
+        if self.dry_run {
+            tracing::info!("dry run: skipping Cargo.toml update");
+            return Ok(());
+        }
+
+        fs::write(cargo_toml_path, new_file).context("failed to write Cargo.toml")?;
+        tracing::info!("updated {cargo_toml_path}");
+        Ok(())
     }
 }
 
@@ -123,17 +163,13 @@ fn metadata_for_crate(crate_name: &str, opts: &cli::Options) -> Result<cargo_met
 // === impl Increment ===
 
 impl Increment {
-    fn increment(&self, current: semver::Version) -> semver::Version {
-        let semver::Version {
-            major,
-            minor,
-            patch,
-            ..
-        } = current;
+    fn increment(&self, current: &semver::Version) -> semver::Version {
         let next = match self {
-            Increment::Major => semver::Version::new(major + 1, 0, 0),
-            Increment::Minor => semver::Version::new(major, minor + 1, 0),
-            Increment::Patch => semver::Version::new(major, minor, patch + 1),
+            Increment::Major => semver::Version::new(current.major + 1, 0, 0),
+            Increment::Minor => semver::Version::new(current.major, current.minor + 1, 0),
+            Increment::Patch => {
+                semver::Version::new(current.major, current.minor, current.patch + 1)
+            }
         };
         tracing::info!(%current, %next, "Incremented version");
         next
