a6c4200236c71b44a4bd10681f5e1df1bfea8d30
Eliza Weisman <eliza@buoyant.io>
2022-09-03 12:09:26 -0700

    add a `force_advance` method to `Timer`

--- maitake/src/timer.rs
+++ maitake/src/timer.rs
@@ -1,7 +1,7 @@
 //! Timer utilities.
 use crate::loom::sync::{
     atomic::{AtomicUsize, Ordering::*},
-    spin::Mutex,
+    spin::{Mutex, MutexGuard},
 };
 use core::{pin::Pin, ptr};
 mod sleep;
@@ -46,8 +46,25 @@ impl Timer {
         Sleep::new(&self.core, ticks)
     }
 
-    /// Advance the timer by `ticks`, waking any `Sleep` futures that have
-    /// completed.
+    /// Advance the timer by `ticks`, potentially waking any `Sleep` futures
+    /// that have completed.
+    ///
+    /// # Interrupt Safety
+    ///
+    /// This method will *never* spin if the timer wheel lock is held; instead,
+    /// it will add any new ticks to a counter of "pending" ticks and return
+    /// immediately. Therefore, it is safe to call this method in an interrupt
+    /// handler, as it will never acquire a lock that may already be locked.
+    ///
+    /// The [`force_advance`] method will spin to lock the timer wheel lock if
+    /// it is currently held, *ensuring* that any pending wakeups are processed.
+    /// That method should never be called in an interrupt handler.
+    ///
+    /// If a timer is driven primarily by calling `advance` in an interrupt
+    /// handler, it may be desirable to occasionally call [`force_advance`]
+    /// *outside* of an interrupt handler (i.e., as as part of an occasional
+    /// runtime bookkeeping process). This ensures that any pending ticks are
+    /// observed by the timer in a relatively timely manner.
     #[inline]
     pub fn advance(&self, ticks: Ticks) {
         // `advance` may be called in an ISR, so it can never actually spin.
@@ -55,14 +72,7 @@ impl Timer {
         // another core, or if a `Sleep` future is currently canceling itself),
         // we just add to a counter of pending ticks, and bail.
         if let Some(mut core) = self.core.try_lock() {
-            // take any pending ticks.
-            let pending_ticks = self.pending_ticks.swap(0, AcqRel) as Ticks;
-            // we do two separate `advance` calls here instead of advancing once
-            // with the sum, because `ticks` + `pending_ticks` could overflow.
-            if pending_ticks > 0 {
-                core.advance(pending_ticks);
-            }
-            core.advance(ticks);
+            self.advance_locked(core, ticks);
         } else {
             // if the core of the timer wheel is already locked, add to the pending
             // tick count, which we will then advance the wheel by when it becomes
@@ -70,6 +80,35 @@ impl Timer {
             // TODO(eliza): if pending ticks overflows that's probably Bad News
             self.pending_ticks.fetch_add(ticks as usize, Release);
         }
+    }
+
+    /// Advance the timer by `ticks`, ensuring any `Sleep` futures that have
+    /// completed are woken, even if a lock must be acquired.
+    ///
+    /// # Interrupt Safety
+    ///
+    /// This method will spin to acquire the timer wheel lock if it is currently
+    /// held elsewhere. Therefore, this method must *NEVER* be called in an
+    /// interrupt handler!
+    ///
+    /// If a timer is advanced inside an interrupt handler, use the [`advance`]
+    /// method instead. If a timer is advanced primarily by calls to
+    /// [`advance`], it may be desirable to occasionally call `force_advance`
+    /// outside an interrupt handler, to ensure that pending ticks are drained
+    /// frequently.
+    pub fn force_advance(&self, ticks: Ticks) {
+        self.advance_locked(self.core.lock(), ticks)
+    }
+
+    fn advance_locked(&self, core: MutexGuard<'_, Core>, ticks: Ticks) {
+        // take any pending ticks.
+        let pending_ticks = self.pending_ticks.swap(0, AcqRel) as Ticks;
+        // we do two separate `advance` calls here instead of advancing once
+        // with the sum, because `ticks` + `pending_ticks` could overflow.
+        if pending_ticks > 0 {
+            core.advance(pending_ticks);
+        }
+        core.advance(ticks);
     }
 }
 
