c0f0f6fe4c6cf3526cd329210486a57cf4dc5c7b
Eliza Weisman <eliza@buoyant.io>
2022-09-04 12:27:31 -0700

    test refactor for reusing utils

--- maitake/src/timer/tests.rs
+++ maitake/src/timer/tests.rs
@@ -1,95 +1,156 @@
 use super::*;
 use crate::scheduler::Scheduler;
-use core::sync::atomic::{AtomicUsize, Ordering};
+use std::collections::BTreeMap;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
 
-fn spawn_sleep_group(
-    scheduler: &Scheduler,
+struct SleepGroupTest {
+    scheduler: Scheduler,
     timer: &'static Timer,
-    count: &'static AtomicUsize,
-    ticks: Ticks,
-) {
-    let tasks = count.load(Ordering::SeqCst);
-    for i in 0..tasks {
-        scheduler.spawn(async move {
-            info!("sleep group task {i} sleeping for {ticks} ticks");
-            timer.sleep(ticks).await;
-            info!("sleep group task {i} slept for {ticks} ticks!");
-            count.fetch_sub(1, Ordering::SeqCst);
-        });
+    now: Ticks,
+    groups: BTreeMap<Ticks, SleepGroup>,
+}
+
+struct SleepGroup {
+    duration: Ticks,
+    t_start: Ticks,
+    tasks: usize,
+    count: Arc<AtomicUsize>,
+}
+
+impl SleepGroupTest {
+    fn new(timer: &'static Timer) -> Self {
+        crate::util::test::trace_init_with_default("info,maitake::timer=trace");
+        Self {
+            scheduler: Scheduler::new(),
+            now: 0,
+            groups: BTreeMap::new(),
+            timer,
+        }
     }
-    info!("spawned {tasks} tasks to sleep for {ticks} ticks");
+
+    fn spawn_group(&mut self, duration: Ticks, tasks: usize) {
+        let count = Arc::new(AtomicUsize::new(tasks));
+        for i in 0..tasks {
+            let count = count.clone();
+            let timer = self.timer;
+            self.scheduler.spawn(async move {
+                info!("sleep group task {i} sleeping for {duration} ticks");
+                timer.sleep(duration).await;
+                info!("sleep group task {i} slept for {duration} ticks!");
+                count.fetch_sub(1, Ordering::SeqCst);
+            });
+        }
+        info!("spawned {tasks} tasks to sleep for {duration} ticks");
+        self.groups.insert(
+            self.now + duration,
+            SleepGroup {
+                duration,
+                t_start: self.now,
+                tasks,
+                count,
+            },
+        );
+    }
+
+    #[track_caller]
+    fn assert(&self) {
+        let t_1 = self.now;
+        for (
+            &t_done,
+            SleepGroup {
+                ref count,
+                duration,
+                tasks,
+                t_start,
+            },
+        ) in self.groups.iter()
+        {
+            let active = count.load(Ordering::SeqCst);
+            let elapsed = t_1 - t_start;
+            if t_done <= t_1 {
+                assert_eq!(
+                    active, 0,
+                    "{tasks} tasks sleeping for {duration} starting at tick \
+                    {t_start} should have completed by tick {t_1} \
+                    ({elapsed} ticks have elapsed)",
+                );
+            } else {
+                assert_eq!(
+                    active, *tasks,
+                    "{tasks} tasks sleeping for {duration} starting at tick \
+                    {t_start} should not have completed by tick {t_1} \
+                    ({elapsed} ticks have elapsed)"
+                );
+            }
+        }
+    }
+
+    #[track_caller]
+    fn advance(&mut self, ticks: Ticks) {
+        let t_0 = self.now;
+        self.now += ticks;
+        // how many tasks are expected to complete?
+        let expected_complete: usize = self
+            .groups
+            .iter_mut()
+            .take_while(|(&t, _)| t <= self.now)
+            .map(|(_, g)| std::mem::replace(&mut g.tasks, 0))
+            .sum();
+
+        // advance the timer.
+        self.timer.advance(ticks);
+
+        let completed = self.scheduler.tick().completed;
+
+        info!(ticks, t_0, t_1 = self.now, completed, "advancing timer");
+
+        self.assert();
+
+        assert_eq!(
+            completed,
+            expected_complete,
+            "expected {expected_complete} tasks to complete when advancing \
+             the timer from {t_0} to {t_1}",
+            t_1 = self.now,
+        );
+    }
 }
 
 #[test]
 fn timer_basically_works() {
-    crate::util::trace_init();
-
     static TIMER: Timer = Timer::new();
-    static SLEEP_GROUP_1: AtomicUsize = AtomicUsize::new(3);
-    static SLEEP_GROUP_2: AtomicUsize = AtomicUsize::new(4);
-    static SLEEP_GROUP_3: AtomicUsize = AtomicUsize::new(5);
-    let scheduler = Scheduler::new();
+    let mut test = SleepGroupTest::new(&TIMER);
 
-    spawn_sleep_group(&scheduler, &TIMER, &SLEEP_GROUP_1, 100);
-    spawn_sleep_group(&scheduler, &TIMER, &SLEEP_GROUP_2, 65535);
-    spawn_sleep_group(&scheduler, &TIMER, &SLEEP_GROUP_3, 6_000_000);
-
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 3);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 4);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    test.spawn_group(100, 2);
+    test.spawn_group(65535, 3);
+    test.spawn_group(6_000_000, 4);
 
     // first tick --- timer is still at zero
-    let tick = scheduler.tick();
+    let tick = test.scheduler.tick();
     assert_eq!(tick.completed, 0);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 3);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 4);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    test.assert();
 
     // advance the timer by 50 ticks.
-    TIMER.advance(50);
-    let tick = scheduler.tick();
-    assert_eq!(tick.completed, 0);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 3);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 4);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    test.advance(50);
 
     // advance the timer by 50 more ticks. the first sleep group should
     // complete.
-    TIMER.advance(50);
-    let tick = scheduler.tick();
-    assert_eq!(tick.completed, 3);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 4);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    test.advance(50);
 
-    TIMER.advance(60000);
-    let tick = scheduler.tick();
-    assert_eq!(tick.completed, 0);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 4);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    // no tasks should complete.
+    test.advance(60000);
 
     // overshoot sleep group 2. the timers should still fire.
-    TIMER.advance(56000);
-    let tick = scheduler.tick();
-    assert_eq!(tick.completed, 4);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    test.advance(56000);
 
-    TIMER.advance(50000);
-    let tick = scheduler.tick();
-    assert_eq!(tick.completed, 0);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 5);
+    // no tasks should complete
+    test.advance(50000);
 
-    TIMER.advance(6_000_000);
-    let tick = scheduler.tick();
-    assert_eq!(tick.completed, 5);
-    assert_eq!(SLEEP_GROUP_1.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_2.load(Ordering::SeqCst), 0);
-    assert_eq!(SLEEP_GROUP_3.load(Ordering::SeqCst), 0);
+    // the last sleep group should complete
+    test.advance(6_000_000);
 }
 
 #[test]
