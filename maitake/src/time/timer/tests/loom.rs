edb7ed61f24df5fc47e1d5e910d2cbec3f7cb6e1
Eliza Weisman <eliza@buoyant.io>
2022-09-10 09:30:09 -0700

    fix loom tests breaking due to API change

--- maitake/src/time/timer/tests/loom.rs
+++ maitake/src/time/timer/tests/loom.rs
@@ -1,6 +1,6 @@
 use super::*;
 use crate::loom::{self, future::block_on, sync::Arc, thread};
-use core::{future::Future, task::Poll};
+use core::{future::Future, task::Poll, time::Duration};
 use futures_util::{future, pin_mut};
 
 #[test]
@@ -9,7 +9,7 @@ fn cancel_polled_sleeps() {
         move || {
             let timer = timer;
             block_on(async move {
-                let sleep = timer.sleep(15);
+                let sleep = timer.sleep_ticks(15);
                 pin_mut!(sleep);
                 future::poll_fn(move |cx| {
                     // poll once to register the sleep with the timer wheel, and
@@ -27,7 +27,7 @@ fn cancel_polled_sleeps() {
     }
 
     loom::model(|| {
-        let timer = Arc::new(Timer::new());
+        let timer = Arc::new(Timer::new(Duration::from_secs(1)));
         let thread = thread::spawn(poll_and_cancel(timer.clone()));
         poll_and_cancel(timer)();
         thread.join().unwrap()
