9406e40e9f6d25d9de0a4de6d4cca9e17276ee16
Eliza Weisman <eliza@buoyant.io>
2022-09-16 10:18:45 -0700

    fixup constructors & stuff

--- maitake/src/scheduler/steal.rs
+++ maitake/src/scheduler/steal.rs
@@ -10,16 +10,28 @@ pub struct Distributor<S> {
     _scheduler_type: PhantomData<fn(S)>,
 }
 
+#[non_exhaustive]
+#[derive(Debug)]
+pub enum TryStealError {
+    Empty,
+    Busy,
+}
+
 impl<S: Schedule> Distributor<S> {
-    pub fn new() -> Self {
-        let stub_task = Box::new(Task::new_stub());
-        let (stub_task, _) =
-            TaskRef::new_allocated::<task::Stub, task::Stub, BoxStorage>(task::Stub, stub_task);
-        Self {
-            queue: MpscQueue::new_with_stub(stub_task),
-            _scheduler_type: PhantomData,
+    loom_const_fn! {
+        /// # Safety
+        ///
+        /// The "stub" provided must ONLY EVER be used for a single
+        /// `Distributor` instance. Re-using the stub for multiple distributors
+        /// or schedulers may lead to undefined behavior.
+        pub unsafe fn new_with_static_stub(stub: &'static TaskStub) -> Self {
+            Self {
+                queue: MpscQueue::new_with_static_stub(&stub.hdr),
+                _scheduler_type: PhantomData,
+            }
         }
     }
+
     pub fn spawn_allocated<STO, F>(&self, task: STO::StoredTask) -> JoinHandle<F::Output>
     where
         F: Future + Send + 'static,
@@ -32,9 +44,9 @@ impl<S: Schedule> Distributor<S> {
         join
     }
 
-    pub fn try_steal(&self, scheduler: &S) -> Result<usize, ()> {
+    pub fn try_steal(&self, scheduler: &S) -> Result<usize, TryStealError> {
         let mut stolen = 0;
-        let tasks = self.queue.try_consume().ok_or(())?;
+        let tasks = self.queue.try_consume().ok_or(TryStealError::Busy)?;
         for task in tasks {
             // TODO(eliza): probably handle cancelation by throwing out canceled
             // tasks here before binding them?
@@ -55,6 +67,17 @@ feature! {
     use super::{BoxStorage, Task};
 
     impl<S: Schedule> Distributor<S> {
+        pub fn new() -> Self {
+            let stub_task = Box::new(Task::new_stub());
+            let (stub_task, _) =
+                TaskRef::new_allocated::<task::Stub, task::Stub, BoxStorage>(task::Stub, stub_task);
+            Self {
+                queue: MpscQueue::new_with_stub(stub_task),
+                _scheduler_type: PhantomData,
+            }
+
+        }
+
         /// Spawns a new task with this builder's configured settings.
         ///
         /// This method returns a [`JoinHandle`] that can be used to await the
@@ -69,6 +92,12 @@ feature! {
         {
             let task = Box::new(Task::<S, _, BoxStorage>::new(future));
             self.spawn_allocated::<BoxStorage, _>(task)
+        }
+    }
+
+    impl<S: Schedule> Default for Distributor<S> {
+        fn default() -> Self {
+            Self::new()
         }
     }
 }
--- maitake/src/util.rs
+++ maitake/src/util.rs
@@ -72,6 +72,18 @@ macro_rules! if_no_atomic_u64 {
 macro_rules! loom_const_fn {
     (
         $(#[$meta:meta])*
+        $vis:vis unsafe fn $name:ident($($arg:ident: $T:ty),*) -> $Ret:ty $body:block
+    ) => {
+        $(#[$meta])*
+        #[cfg(not(loom))]
+        $vis const unsafe fn $name($($arg: $T),*) -> $Ret $body
+
+        $(#[$meta])*
+        #[cfg(loom)]
+        $vis unsafe fn $name($($arg: $T),*) -> $Ret $body
+    };
+    (
+        $(#[$meta:meta])*
         $vis:vis fn $name:ident($($arg:ident: $T:ty),*) -> $Ret:ty $body:block
     ) => {
         $(#[$meta])*
