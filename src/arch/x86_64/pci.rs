85b062005dd626184eff46599200843738bfd348
Eliza Weisman <eliza@buoyant.io>
2022-10-10 10:11:26 -0700

    feat(kernel): prettier traces when enumerating PCI (#339)

--- src/arch/x86_64/pci.rs
+++ src/arch/x86_64/pci.rs
@@ -2,18 +2,23 @@ pub fn init_pci() {
     // TODO(eliza): store the enumerated PCI devices somewhere lol
     let mut found = 0;
     let mut bad = 0;
-    tracing::info!("enumerating PCI devices...");
+    let _span = tracing::info_span!("enumerating PCI devices").entered();
     for (addr, config) in mycelium_pci::config::enumerate_all() {
-        tracing::info!(
-            target: " pci",
-            vendor = config.header.id.vendor_id,
-            device = config.header.id.device_id,
-            "[{addr}]"
-        );
         match config.header.class() {
-            Ok(class) => tracing::info!(target: " pci", class = %class),
+            Ok(class) => tracing::info!(
+                target: "pci",
+                vendor = config.header.id.vendor_id,
+                device = config.header.id.device_id,
+                %class,
+                "[{addr}]"
+            ),
             Err(error) => {
-                tracing::warn!(target: "pci", error = %error, "bad class");
+                tracing::warn!(target: "pci",
+                    error = %error,
+                    vendor = config.header.id.vendor_id,
+                    device = config.header.id.device_id,
+                    "[{addr}] bad class"
+                );
                 bad += 1;
             }
         }
