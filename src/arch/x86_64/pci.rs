1571ba77f3678b051f7efb8495215ca35c49daaf
Eliza Weisman <eliza@buoyant.io>
2022-12-07 09:58:01 -0800

    feat(x86_64): store PCI devices in a registry (#380)

--- src/arch/x86_64/pci.rs
+++ src/arch/x86_64/pci.rs
@@ -1,35 +1,21 @@
 // TODO(eliza): write a `RwLock`...
-use alloc::collections::{BTreeMap, BTreeSet};
-pub use mycelium_pci::*;
+use crate::drivers::pci::*;
 
-#[derive(Debug)]
-pub struct DeviceRegistry {
-    by_class: BTreeMap<Class, Devices>,
-    by_vendor: BTreeMap<u16, BTreeMap<u16, Devices>>,
-    len: usize,
-}
-
-#[derive(Clone, Debug, Default)]
-pub struct Devices(BTreeSet<Address>);
-
-pub fn init_pci() -> DeviceRegistry {
+pub fn init_pci() {
     let mut bad = 0;
-    let mut devices = DeviceRegistry {
-        by_class: BTreeMap::new(),
-        by_vendor: BTreeMap::new(),
-        len: 0,
-    };
+    let mut devices = DeviceRegistry::default();
 
     let _span = tracing::info_span!("enumerating PCI devices").entered();
     for (addr, config) in config::enumerate_all() {
+        let id = config.header.id;
         let class = match config.header.class() {
             Ok(class) => class,
             Err(error) => {
                 tracing::warn!(
                     target: "pci",
                     error = %error,
-                    vendor = config.header.id.vendor_id,
-                    device = config.header.id.device_id,
+                    vendor = id.vendor_id,
+                    device = id.device_id,
                     "[{addr}] bad class"
                 );
                 bad += 1;
@@ -39,24 +25,19 @@ pub fn init_pci() -> DeviceRegistry {
 
         tracing::info!(
             target: "pci",
-            vendor = config.header.id.vendor_id,
-            device = config.header.id.device_id,
+            vendor = id.vendor_id,
+            device = id.device_id,
             %class,
             "[{addr}]"
         );
-        devices.devices.len += 1;
+        assert!(
+            !devices.insert(addr, class, id),
+            "PCI device inserted twice! device={:?}",
+            config
+        );
     }
 
-    tracing::info!("found {} PCI devices ({bad} bad)", devices.len);
+    tracing::info!("found {} PCI devices ({bad} bad)", devices.len());
 
-    devices
-}
-
-impl DeviceRegistry {
-    pub fn insert(&mut self, addr: Address, class: Class, id: device::Id) -> bool {
-        self.by_class
-            .entry(class)
-            .or_insert(Devices::default())
-            .insert(addr);
-    }
+    DEVICES.init(devices);
 }
