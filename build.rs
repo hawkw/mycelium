40f5bdec720a1751c3d2c57e5e694f842b485c5d
Eliza Weisman <eliza@buoyant.io>
2022-12-12 09:28:26 -0800

    refac(build): factor out WAST compilation (#382)
    This moves WAST compilation out into a separate function in the build
    script, and adds improved error handling.

--- build.rs
+++ build.rs
@@ -1,15 +1,34 @@
+use color_eyre::{
+    eyre::{eyre, WrapErr},
+    Result,
+};
 use std::env;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> color_eyre::Result<()> {
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
 
-    // Build our helloworld.wast into binary.
-    let binary = wat::parse_file("src/helloworld.wast")?;
-    fs::write(out_dir.join("helloworld.wasm"), binary)?;
+    let wasm_path = PathBuf::from("src/asdf.wast");
+    build_wasm(&wasm_path, &out_dir)
+        .with_context(|| format!("building WASM `{}` failed!", wasm_path.display()))?;
 
-    println!("cargo:rerun-if-changed=src/helloworld.wast");
     println!("cargo:rerun-if-changed=x86_64-mycelium.json");
+    Ok(())
+}
+
+fn build_wasm(wasm: impl AsRef<Path>, out_dir: impl AsRef<Path>) -> Result<()> {
+    let wasm = wasm.as_ref();
+    // Build our helloworld.wast into binary.
+    let binary = wat::parse_file(wasm)?;
+    let file_stem = wasm
+        .file_stem()
+        .ok_or_else(|| eyre!("wast file path has no file stem"))?;
+    fs::write(
+        out_dir.as_ref().join(file_stem).with_extension("wasm"),
+        binary,
+    )?;
+
+    println!("cargo:rerun-if-changed={}", wasm.display());
     Ok(())
 }
