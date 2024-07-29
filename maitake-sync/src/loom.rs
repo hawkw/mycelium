54984d62e6042ee00f4562a5963335bb21981519
Eliza Weisman <eliza@elizas.website>
2024-07-29 08:50:01 -0700

    fix `loom` version of `with_raw_mutex`

--- maitake-sync/src/loom.rs
+++ maitake-sync/src/loom.rs
@@ -105,7 +105,7 @@ mod inner {
                 }
 
                 #[track_caller]
-                pub(crate) fn with_raw_mutex(t: T) -> Self {
+                pub(crate) fn with_raw_mutex(t: T, _: Lock) -> Self {
                     Self::new(t)
                 }
 
