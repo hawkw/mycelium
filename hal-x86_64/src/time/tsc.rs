9804a97b8b93b0f8abee4ea6037dbe512d490a71
Eliza Weisman <eliza@elizas.website>
2024-12-28 10:17:42 -0800

    WIP CPUID TSC stuff

--- hal-x86_64/src/time/tsc.rs
+++ hal-x86_64/src/time/tsc.rs
@@ -2,12 +2,16 @@ use crate::cpu::{intrinsics, FeatureNotSupported};
 use core::{
     sync::atomic::{AtomicU32, Ordering::*},
     time::Duration,
+    u64,
 };
 use maitake::time::Clock;
 use raw_cpuid::CpuId;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Rdtsc(());
+
+#[derive(Clone, Debug)]
+pub struct TscInitError(&'static str);
 
 impl Rdtsc {
     pub fn is_supported() -> bool {
@@ -36,12 +40,101 @@ impl Rdtsc {
         }
     }
 
+    fn cpuid_frequency_hz() -> Result<u64, TscInitError> {
+        let cpuid = CpuId::new();
+
+        // First, let's check if we're in a VM...
+        if let Some(info) = cpuid.get_hypervisor_info() {
+            let hypervisor = info.identify();
+            if let Some(freq_khz) = info.tsc_frequency() {
+                tracing::info!(
+                    ?hypervisor,
+                    "hypervisor reports TSC frequency is {freq_khz} kHz"
+                );
+                return Ok(freq_khz as u64 * 1_000);
+            } else {
+                // Just keep going.
+                tracing::info!(?hypervisor, "hypervisor does not report TSC frequency");
+            }
+        }
+
+        // EDX[8] in CPUID leaf 0x8000_0007 (APM Info) is "TscInvariant", which
+        // indicates that the TSC runs at a constant rate regasrdless of ACPI
+        // P-, C-, and T-states.
+        let invariant = cpuid
+            .get_advanced_power_mgmt_info()
+            .ok_or(TscInitError("missing CPUID leaf 0x8000_0007"))?
+            .has_invariant_tsc();
+        if !invariant {
+            // If the TSC is not invariant, we won't use it as the maitake timer
+            // clock source.
+            return Err(TscInitError("TSC is not invariant"));
+        }
+
+        // The CPUID leaves and MSRs that we'll look at to determine the TSC
+        // frequency depend on the CPU vendor.
+        let vendor = cpuid
+            .get_vendor_info()
+            .ok_or(TscInitError("no CPUID vendor string"))?;
+        let vendor = vendor.as_str();
+
+        if vendor == "GenuineIntel" {
+            // CPUID Leaf 0x15, Time Stamp Counter/Core Crystal Clock Information
+            let tsc_info = cpuid
+                .get_tsc_info()
+                .ok_or(TscInitError("intel CPU missing CPUID leaf 0x15 TSC info"))?;
+            // N.B.: `raw_cpuid`'s `TscInfo` struct has a `tsc_frequency()`
+            // method that *basically* does this, but it doesn't know about
+            // Denverton reporting a 0Hz crystal frequency, so we'll do it
+            // ourselves to handle that.
+            let crystal_hz = tsc_info.nominal_frequency() as u64;
+            if crystal_hz == 0 {
+                // TODO(eliza): Denverton SoCs will have a 0 here, so we should
+                // check for those parts and set this to 25,000 kHz.
+                // As seen in linux:
+                // https://github.com/torvalds/linux/blob/fd0584d220fe285dc45be43eede55df89ad6a3d9/arch/x86/kernel/tsc.c#L681-L688
+                return Err(TscInitError("intel CPU has unknown crystal frequency"));
+            }
+
+            let numerator = tsc_info.numerator() as u64;
+            let denominator = tsc_info.denominator() as u64;
+            if numerator == 0 || denominator == 0 {
+                return Err(TscInitError("intel CPU has invalid TSC ratio"));
+            }
+
+            return Ok(crystal_hz * numerator / denominator);
+        }
+
+        if vendor == "AuthenticAMD" {
+            return Err(TscInitError(
+                "AMD TSC frequency detection not implemented yet",
+            ));
+        }
+
+        Err(TscInitError(
+            "CPU vendor is 'some other shit', get a real computer",
+        ))
+    }
+
     /// Returns a [`maitake::time::Clock`] defining a clock that uses `rdtsc`
     /// timestamps to produce `maitake` ticks.
-    pub fn into_maitake_clock(self) -> Result<Clock, &'static str> {
+    pub fn into_maitake_clock(self) -> Result<Clock, TscInitError> {
         // TODO(eliza): fix this
         #![allow(unreachable_code)]
-        return Err("calibration routine doesn't really work yet, sorry!");
+
+        // First, see if we can figure it out from CPUID.
+        match Self::cpuid_frequency_hz() {
+            Ok(freq_hz) => {
+                tracing::info!("determined TSC frequency via CPUID: {freq_hz} Hz");
+            }
+            Err(TscInitError(error)) => {
+                tracing::warn!(%error, "could not determine TSC frequency from CPUID!");
+            }
+        };
+
+        return Err(TscInitError(
+            "calibration routine doesn't really work yet, sorry!",
+        ));
 
         const NOT_YET_CALIBRATED: u32 = u32::MAX;
 
@@ -90,11 +183,17 @@ impl Rdtsc {
                 tracing::info!(?tick_duration, shift, "calibrated RDTSC Maitake clock");
                 MAITAKE_TICK_SHIFT
                     .compare_exchange(NOT_YET_CALIBRATED, shift, AcqRel, Acquire)
-                    .map_err(|_| "RDTSC Maitake clock has already been calibrated")?;
+                    .map_err(|_| TscInitError("RDTSC Maitake clock has already been calibrated"))?;
                 return Ok(Clock::new(tick_duration, now).named("CLOCK_RDTSC"));
             } else {
                 shift += 1;
             }
         }
+    }
+}
+
+impl core::fmt::Display for TscInitError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
